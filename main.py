25470313390a2d815a8fa1a9d365b7405432b1ad
Aprendiz Ecommerce 2 <Aprendizeco2@siju.local>
2026-01-23 08:39:54 -0500

    feat se crea diccionario

---
+++ main.py
@@ -0,0 +1,17 @@
+contador=1
+'''while contador<5:
+    print(f"estoy en la vuelta: {contador}")
+    contador=contador+1'''
+ 
+
+for _ in range (1,6):
+
+    diccionario={}
+    diccionario["id"]=int(input("Digita el id del empleado"))
+    id=int(input("Digigta el id del empleado: "))
+    diccionario["nombres"]=(input("Digita los nombre del empleado: "))
+    nombres=input("Digita los nombre del empleado: ")
+    diccionario["documento"]=(input("Digita el documento: "))
+    documento=input("Digita el documento: ")
+    corrreo=input("Digita el correo: ")
+    contraseña=input("Digita la contraseña: ")
